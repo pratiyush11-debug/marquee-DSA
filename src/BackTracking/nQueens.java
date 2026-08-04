e2c348e3b1772492f5e5a0c7c8ac9416ac306e5f
pratiyush11-debug <pratiyushsingh2006@gmail.com>
2026-08-05 01:17:47 +0530

    4-august

---
+++ src/BackTracking/nQueens.java
@@ -0,0 +1,90 @@
+package BackTracking;
+
+public class nQueens {
+    public static void main(String[] args) {
+        int n = 6;
+        char[][] board = new char[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                board[i][j] = 'X';
+            }
+        }
+        nqueen(board, 0);
+
+
+    }
+
+    private static void nqueen(char[][] board, int row) {
+        int n = board.length;
+        if (row == n) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    System.out.print(board[i][j]);
+                }
+                System.out.println();
+            }
+            System.out.println();
+            return;
+
+
+        }
+        for (int j = 0; j < n; j++) {
+            if (isSafe(board, row, j)) {
+                board[row][j] = 'Q';
+                nqueen(board, row + 1);
+                board[row][j] = 'X';//backtracking
+            }
+
+
+        }
+
+    }
+
+    private static boolean isSafe(char[][] board, int row, int col) {
+      //check row
+        int n=board.length;
+        for(int j=0;j<n;j++){
+            if(board[row][j]=='Q') return false;
+
+        }
+        //check col
+        for(int i=0;i<n;i++){
+            if(board[i][col]=='Q') return false;
+
+        }
+        //check north east
+        int i=row;
+        int j=col;
+        while(i>=0 && j<n){
+            if(board[i][j]=='Q')return false;
+            i--;
+            j++;
+        }
+        // check south east
+        i=row;
+        j=col;
+        while(i<n && j<n){
+            if(board[i][j]=='Q')return false;
+            i++;
+            j++;
+        }
+        i=row;
+        j=col;
+        //check south west
+        while(i<n && j>=0){
+            if(board[i][j]=='Q')return false;
+            i++;
+            j--;
+        }
+        i=row;
+        j=col;
+        //check north west
+        while(i>=0 && j>=0){
+            if(board[i][j]=='Q')return false;
+            i--;
+            j--;
+        }
+        return true;
+
+    }
+}
---
+++ src/BackTracking/permutations.java
@@ -0,0 +1,26 @@
+package BackTracking;
+
+public class permutations {
+    public static  void printp(String str,String t){
+        if(str.equals("")){
+            System.out.println(t);
+            return;
+        }
+        for(int i=0;i<str.length();i++){
+            char ch=str.charAt(i);
+            String left= str.substring(0,i);
+            String right=str.substring(i+1);
+            String rem=left +right;
+            printp(rem,t+ch);
+
+        }
+
+    }
+    public static void main(String[] args) {
+        String str="abc";
+        String t="";
+        printp(str,"");
+
+
+    }
+}
